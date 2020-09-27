abcc172b242676bc9bb963fa36455e379687a4a3
JenniferAbouJaoudi <43418483+JenniferAbouJaoudi@users.noreply.github.com>
2020-09-27 10:20:29 -0400

    First commit for the Player Part.
    First commit for the Player Part.

---
+++ Warzone School Project/Warzone School Project/src/Part 3/Player.h
@@ -0,0 +1,39 @@
+//include libraries
+#include <iostream>
+#include <string>
+#include <vector>
+
+// include parts 
+#include "Map.h"
+#include "Cards.h"
+#include "Territory.h"
+#include "Orders.h"
+
+class Territory; 
+class Order; 
+
+class Player {
+private:
+	vector<Territory*> territorries;
+	Hand* hand;
+public:
+	Player() {}
+	~Player();
+	Player(string, vector<Territory*>);
+
+	//Getters
+	vector<Territory*> getTerritorries();
+	Territory* getTerritory(string n);
+	Hand* getHand();
+
+	//Setters
+	void setTerritorries();
+	void setHand();
+
+	//Methods 
+	void addTerritory(Territory* newTerritory);
+	void removeTerritory(Territory* oldTerritory);
+	void toDefend(Player* p, Territory* t);
+	void toAttack()(Player* p, Territory* t);
+	void issueOrder();
+};
