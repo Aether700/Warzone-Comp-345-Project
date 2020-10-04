1fcea9153d965aefa587661b6c47deb61706f513
khalilalturk <khalil.alturk.ka@gmail.com>
2020-10-04 12:25:48 -0400

    added the operators and copy constructors

--- Warzone School Project/Warzone School Project/src/Assignment 1/Cards.h
+++ Warzone School Project/Warzone School Project/src/Assignment 1/Cards.h
@@ -4,10 +4,11 @@
 #define CARDS_H_
 
 #include <vector>
+#include "Orders.h"
 
 namespace WZ
 {
-    class Order;
+    
     class Territory;
     class Player;
     
@@ -16,42 +17,57 @@ namespace WZ
     public:
         Card();
         ~Card();
+        Card(const Card& other);
         void setType(int random);
         const char* getType() const;
+        Order* play(Territory* start,Territory* dest,Player* p,Player* r);
+        Card& operator=(const Card& other);
     private:
         const char* type;
-        Order* play(Territory* start,Territory* dest,Player* p);
     };
+    std::ostream& operator<<(std::ostream& stream, const Card& c);
 
     class Deck
     {
     public:
         Deck();
         ~Deck();
+        Deck(const Deck& other);
         void initDeck(Deck* deck);
         void addCardToDeck(Card* card);
         void removeCardFromDeck(Card* card);
         Card* draw();
+        Deck& operator=(const Deck& other);
+        std::vector<Card*>::iterator begin();
+        std::vector<Card*>::iterator end();
+        std::vector<Card*>::const_iterator begin() const;
+        std::vector<Card*>::const_iterator end() const;
 
     private:
         std::vector<Card*> deck;
 
     };
-
-
-
+    std::ostream& operator<<(std::ostream& stream, const Deck& d);
 
     class Hand {
 
     public:
         Hand();
         ~Hand();
+        Hand(const Hand& other);
         void addCardToHand(Card* card1);
         void removeCardFromHand(Card* card1);
+        Hand& operator=(const Hand& other);
+        std::vector<Card*>::iterator begin();
+        std::vector<Card*>::iterator end();
+        std::vector<Card*>::const_iterator begin() const;
+        std::vector<Card*>::const_iterator end() const;
+        
 
     private:
         std::vector<Card*> hand;
     };
+    std::ostream& operator<<(std::ostream& stream, const Hand& h);
 }
 
 #endif /* CARDS_H_ */
